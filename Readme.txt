93b14d7bde3ca9ef08d93f25433b0757d204d211
Raymond Yang <v-rayyang@microsoft.com>
2023-04-06 22:36:13 +0800

    added

---
+++ Readme.txt
@@ -0,0 +1,6 @@
+Raymond's personal vcpkg registry.
+
+--2023-04-06
+
+vcpkg | Creating registries | Git registries
+https://learn.microsoft.com/en-us/vcpkg/maintainers/registries#git-registries
---
+++ ports/ray-cppguid/portfile.cmake
@@ -0,0 +1,9 @@
+# header-only library
+
+vcpkg_from_github (
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO yangrongwei/ray-CppGuid
+    HEAD_REF master
+    REF 49579713d72932e739e3cdb5ef3272983bbab452
+)
+
