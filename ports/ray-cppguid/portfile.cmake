8e4ad6e1e4d7005f59c9bee48caf2938a6725a95
Raymond Yang <v-rayyang@microsoft.com>
2023-04-07 01:49:10 +0800

    fix

--- ports/ray-cppguid/portfile.cmake
+++ ports/ray-cppguid/portfile.cmake
@@ -5,6 +5,6 @@ vcpkg_from_github (
     REPO yangrongwei/ray-CppGuid
     HEAD_REF master
     REF 49579713d72932e739e3cdb5ef3272983bbab452
-    SHA512 0
+    SHA512 2dcf4959eed1bf926fb73f35f4eac1225a45477b2c0410fb0655721828eb12787e647ccc879d3abfd7542d31e3f0cf1f3113f557d1549a7e4c96c10e2e0613ed
 )
 
